840f1123ad7be4dfbfe95b78dd0d3ac3a487b748
Zehua Chen <zehua-chen@outlook.com>
2025-04-21 22:05:05 -0700

    [chore] Separately pack vendor chunks

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,4 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Since we are hosting on Github Pages (which use Git), we need to minimize the
+        // number of files each build changes.
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+
+          return null;
+        },
+      },
+    },
+  },
 });
