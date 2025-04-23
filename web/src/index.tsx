5926d93aeb37db8c6c9bedea7ced5d1791bacd65
Zehua Chen <zehua-chen@outlook.com>
2025-04-22 19:59:20 -0700

    [fix] Fix null check.

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 
 import './index.scss';
 
-const root = createRoot(document.getElementById('app-main'));
+const root = createRoot(document.getElementById('app-main')!);
 root.render(
   <StrictMode>
     <App />
