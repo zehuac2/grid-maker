2b8ec207590d1cdefa31239476fec5269805dfd6
Zehua Chen <zehua-chen@outlook.com>
2026-01-01 00:02:49 -0800

    Integrate React compiler.

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const ReactCompilerConfig = {};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+      },
+    }),
+  ],
   base: '/grid-maker',
   resolve: {
     alias: {
